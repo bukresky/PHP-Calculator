6548825fee96a71e840c70beace6916a1d2fab67
Jovan Bukreski <79746891+bukresky@users.noreply.github.com>
2025-04-17 13:09:46 +0200

    Add files via upload

---
+++ MyWebsite/formHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+//var_dump($_SERVER["REQUEST_METHOD"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $firstName = htmlspecialchars($_POST["firstname"]);
+
+
+}
+else {
+    header("Location: ../index.php");
+}
